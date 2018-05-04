e098ee8740f99b1000921d4632db1bfd9d8afc43
BucketOJunk <jp_smith96@hotmail.com>
2018-05-04 17:48:38 +0100

    first commit

---
+++ Documents/Projects/Epsilon/Epsilon/EpsilonMain.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Epsilon
+{
+	public class EpsilonMain
+	{
+		public EpsilonMain()
+		{
+		}
+	}
+}
---
+++ Documents/Projects/Epsilon/Epsilon/gtk-gui/Epsilon.Dialog.cs
@@ -0,0 +1,14 @@
+
+namespace Epsilon
+{
+	public partial class Dialog
+	{
+		private Gtk.Button buttonCancel;
+
+		private Gtk.Button buttonOk;
+
+		private void Build()
+		{
+		}
+	}
+}
